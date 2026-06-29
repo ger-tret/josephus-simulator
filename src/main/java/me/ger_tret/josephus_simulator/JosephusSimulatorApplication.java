0c24f6ec03d3a88c77e5d1d7df68acb313bb0439
German Tretyakevich <ger.tretyakevich@gmail.com>
2026-06-29 14:18:56 +0300

    feat: initial project structure with dependencies

---
+++ src/main/java/me/ger_tret/josephus_simulator/JosephusSimulatorApplication.java
@@ -0,0 +1,13 @@
+package me.ger_tret.josephus_simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JosephusSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JosephusSimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/ger_tret/josephus_simulator/JosephusSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ger_tret.josephus_simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JosephusSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
